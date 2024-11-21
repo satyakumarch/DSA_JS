f333f2d522a837c3342ea0f2f3a08f466f0cf926
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-11-21 22:39:38 +0530

    javascript dsa

---
+++ Array/s.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+
+	    int n;
+	    cin>>n;
+	    if(n%2!=0){
+	        cout<<"yes"<<endl;
+	    }else{
+	        
+	            cout<<"no"<<endl;
+	        
+	    }
+	    	
+
+	
+	return 0;
+
+}
